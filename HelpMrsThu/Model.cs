1abafd25c6ea941cbbbfee867ca33600b6252df0
nguyenduyhieu201 <nguyenduyhieu202@gmail.com>
2024-04-08 18:18:41 +0700

    Add project files.

---
+++ HelpMrsThu/Model.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpMrsThu
+{
+    public class RangeTimeModel
+    {
+        public DateTime FromDate { set; get; }
+        public DateTime ToDate { set; get; }
+        public int Row { set; get; }
+    }
+}
